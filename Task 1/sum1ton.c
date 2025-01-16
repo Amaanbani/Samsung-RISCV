b4152c31cfbe81cda5c1b93a46a574404efde423
Amaanbani <amaanbani@gmail.com>
2025-01-16 14:09:00 +0530

    Update sum1ton.c

--- Task 1/sum1ton.c
+++ Task 1/sum1ton.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 int main(){
-    int sum = 0, i, n;
-    printf("Enter the value of n = ");
-    scanf("%d",&n);
-    for(i = 1;i <= n;i++){
-       sum = sum + i;
+    int i, sum = 0, n= 500;
+    for(i = 1;i <= n;++i){
+       sum += i;
     }
     printf("The Sum of numbers from 1 to %d is %d\n",n,sum);
     return 0;
