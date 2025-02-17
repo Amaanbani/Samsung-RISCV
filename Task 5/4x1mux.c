02b3cb70ff8af6a3c96eb0aba0b50c13eb1ffe33
Amaanbani <amaanbani@gmail.com>
2025-02-17 22:30:15 +0530

    Update 4x1mux.c

--- Task 5/4x1mux.c
+++ Task 5/4x1mux.c
@@ -1,68 +1,53 @@
-#include "ch32v00x.h"  // Include CH32V003 RISC-V support
-#include <stdint.h>
+#include <stdio.h>
+#include <debug.h>
+#include <ch32v00x.h>
 
-// Define GPIO pins
-#define D0 GPIO_Pin_0
-#define D1 GPIO_Pin_1
-#define D2 GPIO_Pin_2
-#define D3 GPIO_Pin_3
-#define S0 GPIO_Pin_4
-#define S1 GPIO_Pin_5
-#define Y  GPIO_Pin_6  // Output
+void GPIO_Config(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE); // Enable clock for Port C
 
-void My_GPIO_Init(void) {  // Renamed to avoid conflict with library function
-    // Enable GPIOC clock
-    RCC->APB2PCENR |= RCC_APB2Periph_GPIOC;
-
-    GPIO_InitTypeDef GPIO_InitStructure;
-
-    // Configure Data inputs (D0-D3) as input pull-down
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;  // Input with pull-down resistor
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-
-    GPIO_InitStructure.GPIO_Pin = D0 | D1 | D2 | D3;
+    // Inputs: I0, I1, I2, I3
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
     GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-    // Configure Select inputs (S0, S1) as input pull-down
-    GPIO_InitStructure.GPIO_Pin = S0 | S1;
+    // Control: S0, S1
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
     GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-    // Configure Output (Y) as output push-pull
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;  // Output push-pull
-    GPIO_InitStructure.GPIO_Pin = Y;
+    // Output: Y (MUX Output)
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_Init(GPIOC, &GPIO_InitStructure);
 }
 
-uint8_t Read_MUX_Output(void) {
-    uint8_t sel0 = (GPIOC->INDR & (1 << S0)) ? 1 : 0;
-    uint8_t sel1 = (GPIOC->INDR & (1 << S1)) ? 1 : 0;
-    uint8_t data = 0;
+int main()
+{
+    uint8_t input_value = 0;
+    uint8_t s0, s1;
 
-    // Select the corresponding data pin (D0-D3) based on S0 and S1
-    switch ((sel1 << 1) | sel0) {
-        case 0b00: data = (GPIOC->INDR & (1 << D0)) ? 1 : 0; break;
-        case 0b01: data = (GPIOC->INDR & (1 << D1)) ? 1 : 0; break;
-        case 0b10: data = (GPIOC->INDR & (1 << D2)) ? 1 : 0; break;
-        case 0b11: data = (GPIOC->INDR & (1 << D3)) ? 1 : 0; break;
-    }
+    GPIO_Config();
+    while(1)
+    {
+        // Read control lines
+        s0 = GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_4);
+        s1 = GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_5);
 
-    return data;
-}
+        // Select input based on S0 and S1
+        if (s0 == RESET && s1 == RESET)
+            input_value = GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_0); // I0
+        else if (s0 == RESET && s1 == SET)
+            input_value = GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1); // I1
+        else if (s0 == SET && s1 == RESET)
+            input_value = GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_2); // I2
+        else
+            input_value = GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_3); // I3
 
-void Set_Output(uint8_t value) {
-    if (value) {
-        GPIOC->BSHR = (1 << Y); // Set output high
-    } else {
-        GPIOC->BSHR = (1 << (Y + 16)); // Set output low
-    }
-}
+        // Output selected input to Y
+        GPIO_WriteBit(GPIOC, GPIO_Pin_6, input_value);
 
-int main(void) {
-    SystemInit();  // Initialize system
-    My_GPIO_Init();   // Configure GPIO
-
-    while (1) {
-        uint8_t output = Read_MUX_Output();
-        Set_Output(output);
+        Delay_Ms(1000);// Delay before next check
     }
 }
